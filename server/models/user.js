1d8b1a1d7de7d94af319ff094b66250da1840166
Edwin L <cslasher@gmail.com>
2017-06-18 16:24:24 +0000

    Add users models

--- server/models/user.js
+++ server/models/user.js
@@ -1,12 +1,33 @@
-var mongoose = require("mongoose")
+const mongoose = require("mongoose")
+const validator = require("validator")
 
 var User = mongoose.model('User', {
-    email: {
-        type: String,
-        required: true,
-        trim: true,
-        minlength: 1
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 1,
+    unique: true,
+    validate: {
+      validator: (value) => { return validator.isEmail(value)},
+      message: '{value} is not a valid email'
     }
+  }, 
+  password: {
+    type: String,
+    required: true,
+    minlength: 6
+  },
+  tokens: [{
+    access: {
+      type: String,
+      required: true
+    }, 
+    token: {
+      type: String,
+      required: true
+    }
+  }]
 })
 
 module.exports = {User}
--- server/server.js
+++ server/server.js
@@ -16,99 +16,110 @@ var app = express()
 app.use(bodyParser.json())
 
 app.post('/todos', (req, res) => {
-    var todo = new Todo({
-        text: req.body.text
-    })
-    todo.save().then((doc) => {
-        res.send(doc)
-    }, (err) => {
-        res.status(400).send(err)
-    })
+  var todo = new Todo({
+    text: req.body.text
+  })
+  todo.save().then((doc) => {
+    res.send(doc)
+  }, (err) => {
+    res.status(400).send(err)
+  })
 })
 
 app.get('/todos', (req, res) => {
-    Todo.find().then((todos) => {
-        res.send({
-            todos
-        })
-    }, (e) => {
-        res.status(400).send(e)
-    })
+  Todo.find().then((todos) => {
+    res.send({todos})
+  }, (e) => {
+    res.status(400).send(e)
+  })
 })
 
 app.get('/todos/:id', (req, res) => {
-    var id = req.params.id
+  var id = req.params.id
 
-    if (!ObjectID.isValid(id)) {
-        return res.status(404).send()
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send()
+  }
+
+  Todo.findById(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send()
     }
-    
-    Todo.findById(id).then((todo) => {
-        if (!todo) {
-            return res.status(404).send()
-        }
-        
-        res.status(200).send({todo})
-    }, (e) => {
-        res.status(400).send()
-    })
+
+    res.status(200).send({todo})
+  }, (e) => {
+    res.status(400).send()
+  })
 })
 
 app.delete('/todos/:id', (req, res) => {
-   var id = req.params.id
+  var id = req.params.id
 
-// Invalid ID
-    if (!ObjectID.isValid(id)) {
-        return res.status(404).send()
+  // Invalid ID
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send()
+  }
+
+  Todo.findByIdAndRemove(id).then((todo) => {
+
+    // ID not found
+    if (!todo) {
+      return res.status(404).send()
     }
-    
-    Todo.findByIdAndRemove(id).then((todo) => {
- 
-        // ID not found
-        if (!todo) {
-            return res.status(404).send()
-        }
-        
-        res.status(200).send({todo})
-    }, (e) => {
-        res.status(400).send()
-    })
+
+    res.status(200).send({todo})
+  }, (e) => {
+    res.status(400).send()
+  })
 })
 
 app.patch('/todos/:id', (req, res) => {
-    var id = req.params.id
-    var body = _.pick(req.body, ['text', 'completed'])
-    if (!ObjectID.isValid(id)) {
-        return res.status(404).send()
+  var id = req.params.id
+  var body = _.pick(req.body, ['text', 'completed'])
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send()
+  }
+
+  if (_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime()
+  }
+  else {
+    body.completed = false
+    body.completedAt = null
+  }
+  Todo.findByIdAndUpdate(id, {
+    $set: body
+  }, {
+    new: true
+  }).then((todo) => {
+    //not found
+    if (!todo) {
+      return res.status(404).send()
     }
-    
-    if (_.isBoolean(body.completed) && body.completed) {
-        body.completedAt = new Date().getTime()
-    } else {
-        body.completed = false
-        body.completedAt = null
-    }
-    Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
-        //not found
-        if (!todo) {
-            return res.status(404).send()
-        }
-        
-        res.send({todo})
-    }).catch((e) => {
-        res.status(400).send()
-    })
+
+    res.send({todo})
+  }).catch((e) => {
+    res.status(400).send()
+  })
+})
+
+app.post('/users', (req, res) => {
+  var body = _.pick(req.body, ['email', 'password'])
+  var user = new User(body)
+  user.save().then((doc) => {
+    res.send(doc)
+  }, (err) => {
+    res.status(400).send(err)
+  })
 })
 
 app.post('/echo', (req, res) => {
-    res.send(req.body)
-    console.log(JSON.stringify(req.body, null, 2))
+  res.send(req.body)
+  console.log(JSON.stringify(req.body, null, 2))
 })
 
 app.listen(port, () => {
-    console.log(`Starting on port: ${port}`)
+  console.log(`Starting on port: ${port}`)
 })
 
-module.exports = {
-    app
-}
+module.exports = {app}
