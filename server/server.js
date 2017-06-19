59c18b8e2440b3bc33136f9912c829d57041a7d6
Edwin L <cslasher@gmail.com>
2017-06-19 16:53:23 +0000

    Add JWT auth

--- server/server.js
+++ server/server.js
@@ -19,8 +19,8 @@ app.post('/todos', (req, res) => {
   var todo = new Todo({
     text: req.body.text
   })
-  todo.save().then((doc) => {
-    res.send(doc)
+  todo.save().then((todo) => {
+    res.send(todo)
   }, (err) => {
     res.status(400).send(err)
   })
@@ -106,9 +106,11 @@ app.patch('/todos/:id', (req, res) => {
 app.post('/users', (req, res) => {
   var body = _.pick(req.body, ['email', 'password'])
   var user = new User(body)
-  user.save().then((doc) => {
-    res.send(doc)
-  }, (err) => {
+  user.save().then(() => {
+    return user.generateAuthToken()
+  }).then((token) => {
+    res.header('x-auth', token).send(user)
+  }).catch((err) => {
     res.status(400).send(err)
   })
 })
