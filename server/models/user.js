c962af6e1562bf5a195e53c9b31de7d09415ee32
Edwin L <cslasher@gmail.com>
2017-06-22 15:02:40 +0000

    Add POST /users/login

--- server/models/user.js
+++ server/models/user.js
@@ -70,6 +70,26 @@ UserSchema.statics.findByToken = function(token) {
     })
 }
 
+UserSchema.statics.findByCredentials = function(email, password) {
+  var User = this
+  
+  return User.findOne({email}).then((user) => {
+    if (!user) {
+      return Promise.reject()
+    }
+    
+    return new Promise((resolve, reject) => {
+      bcrypt.compare(password, user.password, (err, res) => {
+        if (res) {
+          return resolve(user)
+        } else {
+          return reject()
+        }
+      })
+    })
+  })
+}
+
 UserSchema.pre('save', function(next) {
     var user = this
   
--- server/server.js
+++ server/server.js
@@ -130,6 +130,18 @@ app.get('/users/me', authenticate, (req, res) => {
     })
 })
 
+app.post('/users/login', (req, res) => {
+  var body = _.pick(req.body, ['email', 'password'])
+  
+  User.findByCredentials(body.email, body.password).then((user) => {
+    user.generateAuthToken().then((token) => {
+      res.header('x-auth', token).send(user)
+    })
+  }).catch((e) => {
+    res.status(400).send()
+  })
+})
+
 app.post('/echo', (req, res) => {
     res.send(req.body)
 })
