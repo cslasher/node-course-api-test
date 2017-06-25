b45ce93053ee6bf82c59ab9650ef99e0d7549a57
Edwin L <cslasher@gmail.com>
2017-06-25 15:23:07 +0000

    Add DELETE /users/me/token

--- server/server.js
+++ server/server.js
@@ -142,6 +142,14 @@ app.post('/users/login', (req, res) => {
   })
 })
 
+app.delete('/users/me/token', authenticate, (req, res) => {
+  req.user.removeToken(req.token).then(() => {
+    res.status(200).send()
+  }, () => {
+    res.status(400).send
+  })
+})
+
 app.post('/echo', (req, res) => {
     res.send(req.body)
 })
